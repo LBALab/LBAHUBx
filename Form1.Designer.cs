b3a0a01aa32984b4360073fb0c5e5275ab3988e5
P-h-r-e-a-k <matt_a_nice_guy_2000@yahoo.co.uk>
2020-07-27 17:44:23 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace LBAHUBx
+{
+    partial class HUD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hud1 = new LBAHUD.HUD();
+            this.SuspendLayout();
+            // 
+            // hud1
+            // 
+            this.hud1.BackColor = System.Drawing.Color.MistyRose;
+            this.hud1.Location = new System.Drawing.Point(1, 0);
+            this.hud1.Name = "hud1";
+            this.hud1.Size = new System.Drawing.Size(638, 20);
+            this.hud1.TabIndex = 0;
+            this.hud1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.hud1_MouseDown);
+            // 
+            // HUD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 20);
+            this.Controls.Add(this.hud1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "HUD";
+            this.ShowIcon = false;
+            this.Text = "LBAHUD";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LBAHUD.HUD hud1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace LBAHUBx
+{
+    public partial class HUD : Form
+    {
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        public HUD()
+        {
+            InitializeComponent();
+            this.TransparencyKey = this.BackColor;
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
+
+        private void hud1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
